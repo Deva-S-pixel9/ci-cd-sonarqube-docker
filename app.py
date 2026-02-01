91cd1eeacf17e550b0090c7664edd6293b11bcf5
Deva-S-pixel9 <devmoorthy26@gmail.com>
2026-02-01 19:54:12 +0530

    Add simple Python app and test

---
+++ app.py
@@ -0,0 +1,7 @@
+def add(a, b):
+    """Function to add two numbers"""
+    return a + b
+
+if __name__ == "__main__":
+    result = add(5, 3)
+    print("5 + 3 =", result)
---
+++ test_app.py
@@ -0,0 +1,7 @@
+from app import add
+
+def test_add():
+    assert add(5, 3) == 8  # checks if the function works
+
+test_add()
+print("Test Passed")
